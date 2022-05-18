544d95dd338a4a057075dc68037d52430bcfaaf3
chris-lock2 <chris.lock1998@gmail.com>
2022-05-17 21:22:52 -0300

    update

--- projeto-site/public/src/main.js
+++ projeto-site/public/src/main.js
@@ -1,4 +1,5 @@
-import { createRoot } from 'react-dom/client'
+// import { createRoot } from 'react-dom/client'
+import createRoot from 'react-router-dom/client'
 import App from './App'
 
 const container = document.getElementById('root')
